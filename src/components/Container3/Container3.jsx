0de667e7aefd83314911eb28f068f0f32e68cb45
Prashant Rastogi <publicidforpeople>
2025-07-20 01:28:07 +0530

    image rendering fix

--- src/components/Container3/Container3.jsx
+++ src/components/Container3/Container3.jsx
@@ -5,28 +5,28 @@ const Container3 = () => {
     <section className="container-3" id="container-3">
       <figure className="figure">
         <a className="anchor" href="https://itpathshaala.in/product-category/cs-course/">
-          <img className="figure-img" src="src/image-container-1/pexels-kevin-ku-92347-577585.jpg" alt="Computer Science & IT" title="Computer Science & IT" />
+          <img className="figure-img" src="images/image-container-1/pexels-kevin-ku-92347-577585.jpg" alt="Computer Science & IT" title="Computer Science & IT" />
           <figcaption className="figure-caption">Computer Science & IT</figcaption>
         </a>
       </figure>
 
       <figure className="figure">
         <a className="anchor" href="https://itpathshaala.in/product-category/accounts-finance/">
-          <img className="figure-img" src="src/image-container-1/pexels-pixabay-53621.jpg" alt="Accounts & Finance" title="Accounts & Finance" />
+          <img className="figure-img" src="images/image-container-1/pexels-pixabay-53621.jpg" alt="Accounts & Finance" title="Accounts & Finance" />
           <figcaption className="figure-caption">Accounts & Finance</figcaption>
         </a>
       </figure>
 
       <figure className="figure">
         <a className="anchor" href="https://itpathshaala.in/product-category/digital-marketing/">
-          <img className="figure-img" src="src/image-container-1/digital-marketing-4111002_1280.jpg" alt="Digital Marketing" title="Digital Marketing" />
+          <img className="figure-img" src="images/image-container-1/digital-marketing-4111002_1280.jpg" alt="Digital Marketing" title="Digital Marketing" />
           <figcaption className="figure-caption">Digital Marketing</figcaption>
         </a>
       </figure>
 
       <figure className="figure">
         <a className="anchor" href="https://itpathshaala.in/product-category/graphic-web-design/">
-          <img className="figure-img" src="src/image-container-1/web-design.jpg" alt="Graphic / Web Design" title="Graphic / Web Design" />
+          <img className="figure-img" src="images/image-container-1/web-design.jpg" alt="Graphic / Web Design" title="Graphic / Web Design" />
           <figcaption className="figure-caption">Graphic / Web Design</figcaption>
         </a>
       </figure>
--- src/components/Container5/Container5.jsx
+++ src/components/Container5/Container5.jsx
@@ -10,28 +10,28 @@ const Container5 = () => {
       <div className="image-container" id="container-5">
         <figure className="figure">
           <a className="anchor" href="https://cadtabs.com/product-category/cadd-course/">
-            <img className="figure-img" src="src/image-container-2/pexels-grovebrands-15764116.jpg" alt="CADD Course" title="CADD Course" />
+            <img className="figure-img" src="images/image-container-2/pexels-grovebrands-15764116.jpg" alt="CADD Course" title="CADD Course" />
             <figcaption className="figure-caption">CADD Course</figcaption>
           </a>
         </figure>
 
         <figure className="figure">
           <a className="anchor" href="https://cadtabs.com/product-category/quantity-surverying/">
-            <img className="figure-img" src="src/image-container-2/pexels-fauxels-3183153.jpg" alt="Quantity Surveying" title="Quantity Surveying" />
+            <img className="figure-img" src="images/image-container-2/pexels-fauxels-3183153.jpg" alt="Quantity Surveying" title="Quantity Surveying" />
             <figcaption className="figure-caption">Quantity Surveying</figcaption>
           </a>
         </figure>
 
         <figure className="figure">
           <a className="anchor" href="https://cadtabs.com/product-category/interior-design/">
-            <img className="figure-img" src="src/image-container-2/home-decoration-property-living-room-room-apartment-945407-pxhere.com.jpg" alt="Interior Design" title="Interior Design" />
+            <img className="figure-img" src="images/image-container-2/home-decoration-property-living-room-room-apartment-945407-pxhere.com.jpg" alt="Interior Design" title="Interior Design" />
             <figcaption className="figure-caption">Interior Design</figcaption>
           </a>
         </figure>
 
         <figure className="figure">
           <a className="anchor" href="https://cadtabs.com/product-category/import-export/">
-            <img className="figure-img" src="src/image-container-2/sea-4106040_1280.jpg" alt="Import Export" title="Import Export" />
+            <img className="figure-img" src="images/image-container-2/sea-4106040_1280.jpg" alt="Import Export" title="Import Export" />
             <figcaption className="figure-caption">Import Export</figcaption>
           </a>
         </figure>
