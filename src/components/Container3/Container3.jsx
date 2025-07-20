12159fef5d0e6bca5fa9b9c4fd4deb86237d903d
Prashant Rastogi <publicidforpeople>
2025-07-20 14:05:37 +0530

    make Figure.jsx file for improving reusability

--- src/components/Container3/Container3.jsx
+++ src/components/Container3/Container3.jsx
@@ -1,35 +1,29 @@
+import Figure from "./Figure";
 import "./style.css";
 
 const Container3 = () => {
   return (
-    <section className="container-3" id="container-3">
-      <figure className="figure">
-        <a className="anchor" href="https://itpathshaala.in/product-category/cs-course/">
-          <img className="figure-img" src="images/image-container-1/pexels-kevin-ku-92347-577585.jpg" alt="Computer Science & IT" title="Computer Science & IT" />
-          <figcaption className="figure-caption">Computer Science & IT</figcaption>
-        </a>
-      </figure>
-
-      <figure className="figure">
-        <a className="anchor" href="https://itpathshaala.in/product-category/accounts-finance/">
-          <img className="figure-img" src="images/image-container-1/pexels-pixabay-53621.jpg" alt="Accounts & Finance" title="Accounts & Finance" />
-          <figcaption className="figure-caption">Accounts & Finance</figcaption>
-        </a>
-      </figure>
-
-      <figure className="figure">
-        <a className="anchor" href="https://itpathshaala.in/product-category/digital-marketing/">
-          <img className="figure-img" src="images/image-container-1/digital-marketing-4111002_1280.jpg" alt="Digital Marketing" title="Digital Marketing" />
-          <figcaption className="figure-caption">Digital Marketing</figcaption>
-        </a>
-      </figure>
-
-      <figure className="figure">
-        <a className="anchor" href="https://itpathshaala.in/product-category/graphic-web-design/">
-          <img className="figure-img" src="images/image-container-1/web-design.jpg" alt="Graphic / Web Design" title="Graphic / Web Design" />
-          <figcaption className="figure-caption">Graphic / Web Design</figcaption>
-        </a>
-      </figure>
+    <section className="container-3">
+      <Figure
+        href="itpathshaala.in/product-category/cs-course"
+        src="image-container-1/pexels-kevin-ku-92347-577585.jpg"
+        text="Computer Science & IT"
+      />
+      <Figure
+        href="itpathshaala.in/product-category/accounts-finance"
+        src="image-container-1/pexels-pixabay-53621.jpg"
+        text="Accounts & Finance"
+      />
+      <Figure
+        href="itpathshaala.in/product-category/digital-marketing"
+        src="image-container-1/digital-marketing-4111002_1280.jpg"
+        text="Digital Marketing"
+      />
+      <Figure
+        href="itpathshaala.in/product-category/graphic-web-design"
+        src="image-container-1/web-design.jpg"
+        text="Graphic / Web Design"
+      />
     </section>
   );
 };
---
+++ src/components/Container3/Figure.jsx
@@ -0,0 +1,20 @@
+export const Figure = ({ href, src, text }) => {
+  return (
+    <>
+      <figure className="figure">
+        <a
+          className="anchor"
+          href={`https://${href}/`}
+        >
+          <img
+            className="figure-img"
+            src={`images/${src}`}
+            alt={text}
+            title={text}
+          />
+          <figcaption className="figure-caption">{text}</figcaption>
+        </a>
+      </figure>
+    </>
+  );
+};
