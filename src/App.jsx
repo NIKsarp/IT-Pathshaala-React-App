7f9e6594c424ff372bec10c09ec531c26454f1b0
Prashant Rastogi <publicidforpeople>
2025-07-30 15:44:11 +0530

    fixing bug render components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,4 @@
-import { Header } from "./Header/Header";
-import { Container1 } from "./Container1/Container1";
-import { Container2 } from "./Container2/Container2";
-import { Container3 } from "./Container3/Container3";
-import { Container4 } from "./Container4/Container4";
-import { Container5 } from "./Container5/Container5";
-import { Container6 } from "./Container6/Container6";
-import { Container7 } from "./Container7/Container7";
-import { Container8 } from "./Container8/Container8";
-import { Container9 } from "./Container9/Container9";
-import { Container10 } from "./Container10/Container10";
-import { Footer } from "./Footer/Footer";
-import { Copyright } from "./Copyright/Copyright";
-
-export default {
+import {
   Header,
   Container1,
   Container2,
@@ -26,4 +12,24 @@ export default {
   Container10,
   Footer,
   Copyright,
+} from "./components/index";
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Container1 />
+      <Container2 />
+      <Container3 />
+      <Container4 />
+      <Container5 />
+      <Container6 />
+      <Container7 />
+      <Container8 />
+      <Container9 />
+      <Container10 />
+      <Footer />
+      <Copyright />
+    </>
+  );
 };
