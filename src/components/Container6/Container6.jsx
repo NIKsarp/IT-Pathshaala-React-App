aed97519ef97e0d6ba4c3f6f56c5f294bb9f2fc0
Prashant Rastogi <publicidforpeople>
2025-08-01 23:45:23 +0530

    add tailwindcss to Container6

--- src/components/Container6/Container6.jsx
+++ src/components/Container6/Container6.jsx
@@ -1,11 +1,9 @@
-import "./style.css";
-
 export const Container6 = () => {
   return (
-    <section className="container-6">
+    <section className="container-6 flex flex-col p-[var(--gap)] text-center text-[clamp(1.5rem,5vw,3rem)] capitalize justify-center items-center ">
       <span>6718 students success story</span>
-      <span>till now</span>
-      <hr className="divider" />
+      <span className="text-[clamp(1rem,5vw,2rem)]">till now</span>
+      <hr className="divider mt-4 w-[clamp(10rem,10vw,30rem)] h-[3px] rounded-[20px] bg-[var(--theme)] " />
     </section>
   );
 };
