beff612949d1bedc13053c5165aa18ddc0e53c8d
Prashant Rastogi <publicidforpeople>
2025-08-01 12:09:48 +0530

    add className for tailwind css and remove style.css file from Container2

--- src/components/Container2/Container2.jsx
+++ src/components/Container2/Container2.jsx
@@ -1,9 +1,9 @@
-import "./style.css";
-
 const Element = ({ title }) => {
   return (
     <>
-      <div className="container-item">{title}</div>
+      <div className="container-item bg-[var(--theme)] text-[#fffffe] p-[var(--gap)] w-[clamp(16rem,10vw,20rem)] rounded-[var(--gap)]">
+        {title}
+      </div>
     </>
   );
 };
@@ -11,7 +11,10 @@ const Element = ({ title }) => {
 export const Container2 = () => {
   return (
     <>
-      <section className="container-2" id="container-2">
+      <section
+        className="container-2 flex flex-wrap gap-[var(--gap)] text-center p-[var(--gap)] justify-center"
+        id="container-2"
+      >
         <Element title="Autodesk Academic Partner" />
         <Element title="Certiport Testing Centre" />
         <Element title="ISO 9001:2015 Certified" />
