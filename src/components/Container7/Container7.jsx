e3332dbe53084453e53c8ed071eeae6c494f0abe
Prashant Rastogi <publicidforpeople>
2025-08-01 23:48:52 +0530

    add tailwindcss to Container7

--- src/components/Container7/Container7.jsx
+++ src/components/Container7/Container7.jsx
@@ -1,13 +1,13 @@
-import "./style.css";
-
 export const Container7 = () => {
   return (
-    <div className="container-7">
+    <div className="container-7 flex text-center flex-col justify-center p-[var(--gap)] ">
       <p className="text">
         Learn Coding, Programming, Website Design & Development, Digital
         Marketing, Graphic Design and Course in Bareilly
       </p>
-      <h2 className="title">Training | Placements | Live Projects</h2>
+      <h2 className="title text-[clamp(1.5rem,5vw,2rem)] text-[var(--theme)]">
+        Training | Placements | Live Projects
+      </h2>
       <p className="text">
         IT Pathshaala is the{" "}
         <strong>Best Training Institute in Bareilly</strong> for
